4894ecbaa70c92b83809608acc5e713519ececaa
Valentin <Alencruserxx@hotmail.fr>
2019-03-07 11:26:11 +0100

    fichiers prêts a l'emploi

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+	public todo;
+	public inputData;
+
+  constructor() {
+  	this.todo = [];
+   }
+
+  ngOnInit() {
+  }
+
+  add() {
+  	this.todo.push(this.inputData);
+  }
+
+  delete(index) {
+  	this.todo.splice(index,1);
+  }
+
+}
